53ae9a4690898834944eb7dae43d3f1382cb9894
Rauane Lima <rauanelima7@gmail.com>
2023-11-26 11:48:15 -0300

    feat: Entity creation

--- MountainShooten/code/Const.py
+++ MountainShooten/code/Const.py
@@ -1,4 +1,6 @@
 # C
+import pygame
+
 COLOR_ORANGE = (214, 93, 50)
 COLOR_GREEN = (49, 71, 89)
 COLOR_YELLOW = (255, 255, 128)
@@ -16,9 +18,27 @@ WIN_WIDTH = 576
 WIN_HEIGHT = 324
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
 ENTITY_SPEED = {'Level1Bg0': 0,
                 'Level1Bg1': 1,
                 'Level1Bg2': 2,
                 'Level1Bg3': 3,
                 'Level1Bg4': 4,
+                'Player1': 3,
+                'Player2': 3,
+                'Enemy1': 2,
+                'Enemy2': 1,
                 }
+# p
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w,
+                 }
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s,
+                   }
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a,
+                   }
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d,
+                    }
--- MountainShooten/code/Level.py
+++ MountainShooten/code/Level.py
@@ -1,8 +1,11 @@
+import random
 import sys
 
 import pygame.display
-from pygame import Surface
+from pygame import Surface, Rect
+from pygame.font import Font
 
+from code.Const import COLOR_WHITE, MENU_OPTION, EVENT_ENEMY
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
 
@@ -15,16 +18,34 @@ class Level:
         self.name = name
 
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        if menu_option in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+        pygame.time.set_timer(EVENT_ENEMY, 4000)
 
     def run(self, ):
+        pygame.mixer_music.load(f'./assets/{self.name}.mp3')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
-                self.window.blit(source=ent.surf, dest=ent.rect)
+                self.window.blit(source=ent.surf, dest=ent.rect)  # aqui desenho entidades
+                # self.level_text(14, f'fps: {clock.get_fps():.0f}', COLOR_WHITE, (10, 10))
                 ent.move()
-            pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
+            pygame.display.flip()
         pass
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Courier New", size=text_size, bold=True)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
